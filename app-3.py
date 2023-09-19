65b969f03fcd340b657c35e4534dd6d718340c58
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-19 22:28:05 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -97,16 +97,17 @@ if st.session_state["messages"]:
     messages = st.session_state["messages"]
     for message in reversed(messages):
         if message["role"] == "user":
-            speaker_icon = "<img src='https://user-images.githubusercontent.com/37874452/268952776-da20fb44-4303-4ebf-9335-9829e31c4f8c.png' width='60' style='vertical-align: middle; float: right;'>"
+            speaker_icon = "<img src='https://user-images.githubusercontent.com/37874452/268952776-da20fb44-4303-4ebf-9335-9829e31c4f8c.png' width='60' style='vertical-align: middle;'>"
             message_align = "flex-end"
             text_align = "right"
+            content_order = f"{speaker_icon}<span style='margin-left: 10px; text-align: {text_align};'>{message['content']}</span>"
         else:
-            speaker_icon = "<img src='https://user-images.githubusercontent.com/37874452/268968551-3cb21d72-8e58-4eb9-894c-697f4b8147a7.png' width='60' style='vertical-align: middle; float: left;'>"
+            speaker_icon = "<img src='https://user-images.githubusercontent.com/37874452/268968551-3cb21d72-8e58-4eb9-894c-697f4b8147a7.png' width='60' style='vertical-align: middle;'>"
             message_align = "flex-start"
-            text_align = "right"
+            text_align = "left"
+            content_order = f"<span style='margin-right: 10px; text-align: {text_align};'>{message['content']}</span>{speaker_icon}"
 
         st.markdown(
-            f"<div style='display: flex; margin-bottom: 20px; justify-content: {message_align}; align-items: center;'><span style='margin-right: 10px; text-align: {text_align};'>{message['content']}</span> {speaker_icon}</div>",
+            f"<div style='display: flex; margin-bottom: 20px; justify-content: {message_align}; align-items: center;'>{content_order}</div>",
             unsafe_allow_html=True,
         )
-
