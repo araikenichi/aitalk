90f5e8830a46edb01ee678a35e3cb31e7e7e7e4e
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-19 22:03:09 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -28,7 +28,7 @@ def communicate():
         messages=messages
     )  
 
-    bot_message = response["choices"][0]["message"]
+    bot_message = response["choices"][0]["messages"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を削除
