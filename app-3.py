ea0358786d0d48df7d6485b42890653192287478
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-19 20:53:24 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -97,22 +97,10 @@ if user_input:
 
 user_icon_url = "https://user-images.githubusercontent.com/37874452/268952776-da20fb44-4303-4ebf-9335-9829e31c4f8c.png"
 
-
 # メッセージの表示
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
-    
-    for message in messages:
-        if message["role"] == "user":
-            speaker = f"<img src='{user_icon_url}' width='60' style='vertical-align: bottom;'>"
-            align = "flex-start"  # ユーザーのメッセージを右寄せ
-        else:
-            speaker = f"<img src='https://user-images.githubusercontent.com/37874452/268891476-c11a2c43-8409-4b14-b770-6e6ba7360ab2.png' width='60' style='vertical-align: bottom;'>"
-            align = "flex-end"  # ボットのメッセージを左寄せ
-        
-        st.markdown(
-            f"<div style='display: flex; align-items: {align}; margin-bottom: 20px;'>"
-            f"{speaker} <span style='margin-left: 10px;'>{message['content']}</span>"
-            f"</div>",
-            unsafe_allow_html=True
-        )
+    for message in reversed(messages):
+        align = "flex-start" if message["role"] == "user" else "flex-end"
+        speaker = f"<img src='{user_icon_url}' width='60' style='vertical-align: bottom;'>" if message["role"] == "user" else f"<img src='{bot_icon_url}' width='60' style='vertical-align: bottom;'>"
+        st.markdown(f"<div style='display: flex; align-items: {align}; margin-bottom: 20px;'>{speaker} <span style='margin-left: 10px;'>{message['content']}</span></div>", unsafe_allow_html=True)
