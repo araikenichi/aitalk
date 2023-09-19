3e8fb9f05cd65dc0aeee4b1e48e1bab70755c159
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-19 21:41:57 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -100,7 +100,7 @@ if st.session_state["messages"]:
         if message["role"] == "user":
             speaker_icon = f"<img src='https://user-images.githubusercontent.com/37874452/268952776-da20fb44-4303-4ebf-9335-9829e31c4f8c.png' width='60' style='vertical-align: bottom; float: right;'>"
             message_align = "left"
-            text_align = "left"  # テキストを左寄せ
+            text_align = "right"  # テキストを右寄せ
         else:
             speaker_icon = f"<img src='https://user-images.githubusercontent.com/37874452/268968551-3cb21d72-8e58-4eb9-894c-697f4b8147a7.png' width='60' style='vertical-align: bottom; float: left;'>"
             message_align = "right"
