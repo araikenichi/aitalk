b508330d5dcb24908ec6ed815cf29fa20a052fcc
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-26 00:38:05 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -4,14 +4,9 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
-
-
 # Session state初期化
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "system", "content": ""}]
-
-
-
 
 # コミュニケーション関数
 def communicate(new_input, language="English"):
@@ -19,92 +14,23 @@ def communicate(new_input, language="English"):
     user_message = {"role": "user", "content": new_input}
     messages.append(user_message)
 
+    # OpenAI APIを使用して応答生成
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
     bot_content = response['choices'][0]['message']['content']
-
-    if language == "English":
-        bot_message = {"role": "assistant", "content": f"Sure, darling! 💕 {bot_content}"}
-    elif language == "日本語":
-        bot_message = {"role": "assistant", "content": f"わかったわ、ちゃんと聞いてるからね！💕 {bot_content}"}
-    elif language == "中文":
-        bot_message = {"role": "assistant", "content": f"明白了，亲爱的！💕 {bot_content}"}
-
+    bot_message = {"role": "assistant", "content": f"{bot_content}"}
     messages.append(bot_message)
     st.session_state["messages"] = messages
 
-
-
-# ...前の部分は変わらず
-
 # UI部分
-st.write("<h1 style='text-align: center;'>LISA</h1>", unsafe_allow_html=True)
-
-# keyをユニークなものに変更
-user_input = st.text_input("message", key="unique_user_input", on_change=communicate)
-
-# ...後の部分も変わらず
-
-
-
-
-
-
-# st.session_stateを使いメッセージのやりとりを保存
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-
-from PIL import Image  # PILライブラリからImageクラスをインポート
-import requests
-from io import BytesIO
-import streamlit as st
-
-
-# 動画のURL
-video_url = "https://user-images.githubusercontent.com/37874452/270353369-38139a9d-2428-454e-956a-23d860d5a6fc.mp4"
-
-st.markdown(
-    f"<div style='text-align: center;'><video width='300' autoplay loop muted><source src='{video_url}' type='video/mp4'></video></div>",
-    unsafe_allow_html=True,
-)
-
-
-
-
-
-
-
-
+st.markdown("<h1 style='text-align: center;'>LISA</h1>", unsafe_allow_html=True)
 
 # ユーザー入力
-user_input = st.text_input("message", key="user_input", on_change=communicate)
-# ...
+user_input = st.text_input("message", key="unique_user_input", on_change=communicate)
 
-# チャットボットとのコミュニケーション
-def communicate(new_input):
-    messages = st.session_state["messages"]
-    user_message = {"role": "user", "content": new_input}
-    messages.append(user_message)
-    
-    # OpenAI APIを使用した応答生成
-      # ...
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )  
-
-    bot_message = response["choices"][0]["message"]
-    # ...
-
-
-
-if user_input:
-    communicate(user_input)
-    st.session_state["user_input"] = ""
-
+# メッセージ表示
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
     for message in reversed(messages):
