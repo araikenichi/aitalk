fa3bf506727e2f44793de4afd2a4eaef8d36b209
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-25 21:31:21 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -48,12 +48,10 @@ import streamlit as st
 # 動画のURL
 video_url = "https://user-images.githubusercontent.com/37874452/270353369-38139a9d-2428-454e-956a-23d860d5a6fc.mp4"
 
-# 動画を中央に表示、サイズは300pxに設定
 st.markdown(
-    f"<div style='text-align: center;'><video width='300' controls><source src='{video_url}' type='video/mp4'></video></div>",
+    f"<div style='text-align: center;'><video width='300' autoplay loop muted><source src='{video_url}' type='video/mp4'></video></div>",
     unsafe_allow_html=True,
 )
-
 
 
 
