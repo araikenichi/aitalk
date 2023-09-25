e38eb77c35ce6ee5f5fb8d551efc254eafc5234b
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-26 00:04:53 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -4,73 +4,9 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
-
-
-# st.session_stateを使いメッセージのやりとりを保存
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": ""}
-        ]
-
-
-
-# チャットボットとやりとりする関数
-def communicate():
-    messages = st.session_state["messages"]
-
-    user_message = {"role": "user", "content": st.session_state["user_input"]}
-    messages.append(user_message)
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )  
-
-    bot_message = response["choices"][0]["message"]
-    messages.append(bot_message)
-
-    st.session_state["user_input"] = ""  # 入力欄を削除
-
-
-# ユーザーインターフェイスの構築
-st.write()
-# タイトルを中央に表示
-st.markdown("<h1 style='text-align: center;'>LISA</h1>", unsafe_allow_html=True)
-
-
-
-
-
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
-
-
-from PIL import Image  # PILライブラリからImageクラスをインポート
-import requests
-from io import BytesIO
-import streamlit as st
-
-
-# 動画のURL
-video_url = "https://user-images.githubusercontent.com/37874452/270353369-38139a9d-2428-454e-956a-23d860d5a6fc.mp4"
-
-st.markdown(
-    f"<div style='text-align: center;'><video width='300' autoplay loop muted><source src='{video_url}' type='video/mp4'></video></div>",
-    unsafe_allow_html=True,
-)
-
-
-
-
-
-
-
-
-
-# ユーザー入力
-user_input = st.text_input("message", key="user_input", on_change=communicate)
-# ...
 
 # チャットボットとのコミュニケーション
 def communicate(new_input):
@@ -86,22 +22,26 @@ def communicate(new_input):
     bot_message = response['choices'][0]['message']['content']
     messages.append({"role": "assistant", "content": bot_message})
 
+# タイトルを中央に表示
+st.markdown("<h1 style='text-align: center;'>LISA</h1>", unsafe_allow_html=True)
+
+# 動画のURL
+video_url = "https://user-images.githubusercontent.com/37874452/270353369-38139a9d-2428-454e-956a-23d860d5a6fc.mp4"
+
+st.markdown(
+    f"<div style='text-align: center;'><video width='300' autoplay loop muted><source src='{video_url}' type='video/mp4'></video></div>",
+    unsafe_allow_html=True,
+)
+
+# ユーザー入力
+user_input = st.text_input("message", key="user_input", on_change=communicate)
 
 # ユーザーが新しいメッセージを入力した場合にcommunicate関数を呼び出す
 if user_input:
     communicate(user_input)
     st.session_state["user_input"] = None  # 入力欄を消去
 
-
-
-
-# 初期化部分
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-
-
-
+# メッセージを表示
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
     for message in reversed(messages):
@@ -122,11 +62,8 @@ if st.session_state["messages"]:
                 <div style='{content_style}'>
                     {content}
                     <div style='{arrow_style}'></div>
-
                 </div>
             </div>
-            """
-            
+            """,
             unsafe_allow_html=True,
         )
-
