799a967cf69b3312a981e4d90f6a2c7dd7f7d051
AK <37874452+araikenichi@users.noreply.github.com>
2023-09-26 06:14:44 +0800

    Update app-3.py

--- app-3.py
+++ app-3.py
@@ -5,34 +5,64 @@ import openai
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 
+# 下部にウィジェットを配置するためのプレースホルダ
+input_placeholder = st.empty()
 
 
 
 
-# チャットボットとやりとりする関数
-def communicate():
-    messages = st.session_state["messages"]
-
-    user_message = {"role": "user", "content": st.session_state["user_input"]}
+# チャットボットとのコミュニケーション
+def communicate(new_input):
+    messages = st.session_state.get("messages", [])
+    user_message = {"role": "user", "content": new_input}
     messages.append(user_message)
-
+    
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
-    )  
-
-    bot_message = response["choices"][0]["message"]
-    messages.append(bot_message)
-
-    st.session_state["user_input"] = ""  # 入力欄を削除
+    )
+    bot_message = response['choices'][0]['message']['content']
+    messages.append({"role": "assistant", "content": bot_message})
+    st.session_state.messages = messages
 
 
+# メッセージの表示
+if st.session_state.get("messages"):
+    messages = st.session_state.get("messages")
+    for message in messages:  # 逆順ではない
+        if message["role"] == "user":
+            message_align = "flex-end"
+            content_style = "background-color: #08A221; color: black; padding: 10px; border-radius: 10px;"
+        else:
+            message_align = "flex-start"
+            content_style = "background-color: #797B79; color: white; padding: 10px; border-radius: 10px;"
+        
+        content_order = f"<span style='{content_style}'>{message['content']}</span>"
+        st.markdown(
+            f"<div style='display: flex; margin-bottom: 20px; justify-content: {message_align}; align-items: center;'>{content_order}</div>",
+            unsafe_allow_html=True,
+        )
+        
 # ユーザーインターフェイスの構築
 st.write()
 # タイトルを中央に表示
 st.markdown("<h1 style='text-align: center;'>LISA</h1>", unsafe_allow_html=True)
 
 
+# ユーザー入力（影を追加するCSSも含む）
+user_input = input_placeholder.text_input("Message", key="user_input", help="Send a message")
+if user_input:
+    communicate(user_input)
+    st.session_state.user_input = ""  # テキストボックスをクリア
+
+# カスタムCSSでテキストボックスに影を追加
+st.markdown("""
+    <style>
+        .stTextInput input {
+            box-shadow: 0px 0px 5px grey;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
 
 
@@ -61,36 +91,12 @@ st.markdown(
 
 
 
-# ユーザー入力
-user_input = st.text_input("message", key="user_input", on_change=communicate)
-# ...
-
-# チャットボットとのコミュニケーション
-def communicate(new_input):
-    messages = st.session_state["messages"]
-    user_message = {"role": "user", "content": new_input}
-    messages.append(user_message)
-    
-    # OpenAI APIを使用した応答生成
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    bot_message = response['choices'][0]['message']['content']
-    messages.append({"role": "assistant", "content": bot_message})
-
-
-# ユーザーが新しいメッセージを入力した場合にcommunicate関数を呼び出す
-if user_input:
-    communicate(user_input)
-    st.session_state["user_input"] = None  # 入力欄を消去
 
 
 
-
-# 初期化部分
+# 初期化
 if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+    st.session_state.messages = []
 
 
 
